41290bd400abbd4acdf3bab9de53dd7de74957f9
ERDENER YAVUZ <erdener.yavuz@gmail.com>
2022-10-16 12:12:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean checkPrime(int number,int index){
+        if(number<=2){
+            return (number==2)?true:false;
+        }
+        if(number % index==0){
+            return false;
+        }
+        if(index*index>number){
+            return true;
+        }
+        return checkPrime(number,index+1);
+    }
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        while(true){
+            System.out.print("Sayı giriniz: ");
+            int number=input.nextInt();
+            if(checkPrime(number,2)){
+                System.out.println(number+" sayısı asaldır!");
+            }else{
+                System.out.println(number+" sayısı  asal değildir!");
+            }
+        }
+    }
+}
